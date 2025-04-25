3c5e1266582bc48df2ba8fbaa26879eb7c81fbee
Tiago Correia <1211742@isep.ipp.pt>
2025-04-25 11:59:20 +0100

    Pipes setup

--- US261/us261.c
+++ US261/us261.c
@@ -4,15 +4,38 @@
 #include <stdio.h>
 #include <string.h>
 
-
+#define DRONE_NUM 50
 
 int main() {
 	
-	int id;
+	int fd[2];
+
+	int p[DRONE_NUM];
+
+	if (pipe(fd) == -1) {
+		perror("Pipe Failed");
+		exit(1);
+	}
+
+	// create childs - drones
+	for (int i = 0; i < DRONE_NUM; i++) {
+
+		p[i] = fork();
+
+		if (p[i] == -1) {
+			perror("Fork Failed");
+			exit(1);
+		}
+
+		if (p[i] == 0) {
+
+			// child/drone process
+
+			exit(0);
+		}
+	}
 	
-	
-	
-	
+	// parent process
 	
 	
 	return 0;
