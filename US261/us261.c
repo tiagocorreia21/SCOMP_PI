8caae942066137abc69d5eafb17b11d4b8199bf9
GabrielBras06 <1211919@isep.ipp.pt>
2025-04-26 15:51:32 +0100

    #1 Update da US261

--- US261/us261.c
+++ US261/us261.c
@@ -6,27 +6,42 @@
 #include <structs.h>
 #include <functions.h>
 
-#define DRONE_NUM 50
+#define POSSIBLE_NODES 10
 #define TIME_STEPS_NUM 200
 #define MAX_COLLISION_NUM 5
 
+typedef struct{
+	int positionDrone;
+	int timeStamp;
+}Position;
+
+
 int main() {
-
-	Position positions[TIME_STEPS_NUM][DRONE_NUM];
-
-	DroneInfo drones[DRONE_NUM];
 	
-	int fd[2];
-
-	int p[DRONE_NUM];
-
-	if (pipe(fd) == -1) {
-		perror("Pipe Failed");
-		exit(1);
+	int numeroDrones;
+	
+	printf("Digite o número de drones para a simulação: ");
+	scanf("%d", &numeroDrones);
+	
+	//Array bidimensional para posições dos drones
+	Position positions[TIME_STEPS_NUM][numeroDrones];
+	
+	
+	int fd[numeroDrones][2];
+	int p[numeroDrones];
+	
+	
+	//Criação de um pipe para cada filho
+	for(int i = 0; i < numeroDrones; i++){
+		if (pipe(fd[i]) == -1) {
+			perror("Pipe Failed");
+			exit(1);
+		}
 	}
-
-	// create childs - drones
-	for (int i = 0; i < DRONE_NUM; i++) {
+	
+	
+	//Cria os filhos(drones)
+	for (int i = 0; i < numeroDrones; i++) {
 
 		p[i] = fork();
 
@@ -36,15 +51,50 @@ int main() {
 		}
 
 		if (p[i] == 0) {
-
-			// child/drone process
-
-			exit(0);
+			
+			close(fd[i][0]);
+			
+			//Tentativa para simular escrita do drone
+            Position pos;
+            
+            for (int t = 0; t < TIME_STEPS_NUM; t++) {
+                pos.positionDrone = i; 
+                pos.timeStamp = t;
+                write(fd[i][1], &pos, sizeof(Position));
+                usleep(50000); //Para simular tempo (50ms)
+            }
+            
+            close(fd[i][1]);
+            exit(0);
+			
+			
 		}
 	}
 	
-	// parent process
+	//Processo pai
 	
+	//Fechar lados de escrita
+    for (int i = 0; i < numeroDrones; i++) {
+        close(fd[i][1]);
+    }
+
+	
+	for (int i = 0; i < TIME_STEPS_NUM; i++){
+		for(int j = 0; j < numeroDrones; j++){
+			
+			Position pos;
+			//Processo pai
+			if(p[j] > 0) {
+				read(fd[j][0], &pos, sizeof(Position));
+				positions[i][j] = pos;
+			}	
+			
+		}
+	}
+	//Fechar os lados da leitura
+	for (int j = 0; j < numeroDrones; j++) {
+		close(fd[j][0]);
+	}
 	
 	return 0;
 }
