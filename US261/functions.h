4259208e922d4d14c064ce46db05889e0bdcb53c
Tiago Correia <1211742@isep.ipp.pt>
2025-05-09 17:58:39 +0100

    #4: Signal handling logic added. Allocate shared memory added.

--- US261/functions.h
+++ US261/functions.h
@@ -1,2 +1,3 @@
 void run_drone_script(int write_fd, int drone_num);
 void initialize_drone_positions(Position *drone_position, int time_step_num, int drone_num);
+shared_data_type *allocate_shared_memory(char *shm_name);
